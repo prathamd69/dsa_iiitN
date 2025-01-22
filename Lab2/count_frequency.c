d83afff02e1001ece63c0f1c5fb471075abad8ea
prathamd69 <prathamd2710@gmail.com>
2025-01-23 03:17:49 +0530

    Update count_frequency.c

--- Lab2/count_frequency.c
+++ Lab2/count_frequency.c
@@ -30,7 +30,7 @@ int main(){
     int m = arr[j];
     int count = 0;
     
-    for (int i = 0; i < n; i++) {
+    for (int i = j; i < n; i++) {
         if (arr[i] == m){
         count++;
             arr[i] = k;
